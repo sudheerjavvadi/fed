fcdeddb48b9cda63a93b040f07e040bcf4108e4f
SUDHEER_JAVVADI <2400030014@kluniversity.in>
2025-11-30 07:20:53 +0530

    final commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: ''   // 👈 MUST MATCH YOUR REPO NAME EXACTLY
 })
